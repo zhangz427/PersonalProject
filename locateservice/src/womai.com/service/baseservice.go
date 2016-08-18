d58bf720854a0a8226b5f89510ea82b80d9a9f66
zheng.zhang <zheng.zhang>
2016-08-18 17:41:06 +0800

    remove locateservice

--- locateservice/src/womai.com/service/baseservice.go
+++
@@ -1,9 +0,0 @@
-package service
-
-type BaseService struct {
-	TrackableLogService
-}
-
-type TrackableLogService struct {
-	LogContext string
-}
--- locateservice/src/womai.com/util/constants.go
+++
@@ -1,7 +0,0 @@
-package util
-
-const (
-	SUCCESS_CODE             = "00"
-	SUCCESS_MESSAGE          = "success"
-	SERVER_ERROR_CODE      = "-91"
-)
